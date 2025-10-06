b93cbed877ae9c7a26d7b667373f4cf0ed2acf0f
deepak rankawat <deepakrankawatcode@gmail.com>
2025-10-06 10:51:55 +0000

    remove  (_for element section_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Services } from '@/components/sections/services';
 import { Team } from '@/components/sections/team';
 import { Testimonials } from '@/components/sections/testimonials';
 import { Blog } from '@/components/sections/blog';
-import { Contact } from '@/components/sections/contact';
 
 export default function HomePage() {
   return (
@@ -17,7 +16,6 @@ export default function HomePage() {
       <Team />
       <Testimonials />
       <Blog />
-      <Contact />
     </main>
   );
 }
