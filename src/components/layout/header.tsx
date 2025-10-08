f61de813afecdb86139c2d5ca893ea9ee9aec44e
deepak rankawat <deepakrankawatcode@gmail.com>
2025-10-08 10:01:45 +0000

    add Home  (_for element div_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,6 +11,7 @@ import { cn } from '@/lib/utils';
 import { Logo } from '@/components/ui/logo';
 
 const navLinks = [
+  { href: '/', label: 'Home' },
   { href: '/about', label: 'About' },
   { href: '/services', label: 'Services' },
   { href: '/lawyer', label: 'Our Team' },
