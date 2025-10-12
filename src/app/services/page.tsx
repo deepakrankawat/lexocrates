ef4e198a0e8b59eae45a6a220f9134a273f0f555
deepak rankawat <deepakrankawatcode@gmail.com>
2025-10-12 13:12:58 +0000

    in service in add

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,13 +1,16 @@
+
 import { ServicesHero } from '@/components/sections/services-hero';
 import { Services } from '@/components/sections/services';
 import { Experience } from '@/components/sections/experience';
 import { Cta } from '@/components/sections/cta';
+import { WhyChooseUs } from '@/components/sections/why-choose-us';
 
 export default function ServicesPage() {
   return (
     <main className="bg-background">
       <ServicesHero />
       <Services />
+      <WhyChooseUs />
       <Experience />
       <Cta />
     </main>
