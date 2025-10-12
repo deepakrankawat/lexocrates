31b143ae97520a463206a47b96e693669461828c
deepak rankawat <deepakrankawatcode@gmail.com>
2025-10-12 13:51:08 +0000

    all webiste in image are use in local pubile folder to

--- next.config.ts
+++ next.config.ts
@@ -10,32 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placeholder.pics',
-        port: '',
-        pathname: '/**',
-      }
-    ],
+    remotePatterns: [],
   },
 };
 
