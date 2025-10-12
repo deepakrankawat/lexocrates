087d2101485ed88401387dd5bded550d35de6ee4
deepak rankawat <deepakrankawatcode@gmail.com>
2025-10-12 17:01:50 +0000

    remove it  (_for element <Pagination>_)

--- src/components/sections/blog-list.tsx
+++ src/components/sections/blog-list.tsx
@@ -52,32 +52,6 @@ export function BlogList() {
             </motion.div>
           ))}
         </div>
-        <div className="mt-16">
-            <Pagination>
-                <PaginationContent>
-                    <PaginationItem>
-                    <PaginationPrevious href="#" />
-                    </PaginationItem>
-                    <PaginationItem>
-                    <PaginationLink href="#" isActive>1</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                    <PaginationLink href="#">
-                        2
-                    </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                    <PaginationLink href="#">3</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                    <PaginationEllipsis />
-                    </PaginationItem>
-                    <PaginationItem>
-                    <PaginationNext href="#" />
-                    </PaginationItem>
-                </PaginationContent>
-            </Pagination>
-        </div>
       </div>
     </section>
   );
