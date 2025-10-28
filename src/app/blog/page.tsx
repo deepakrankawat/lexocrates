eaaf3e42c9303857f01b52fe4bd1ce63b47624c5
deepak rankawat <deepakrankawatcode@gmail.com>
2025-10-28 07:41:00 +0000

    like a i use json animtion in compouent to show animtion

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,14 @@
 import { BlogHero } from '@/components/sections/blog-hero';
 import { BlogList } from '@/components/sections/blog-list';
 import { Cta } from '@/components/sections/cta';
+import { BlogShowcase } from '@/components/sections/blog-showcase';
 
 export default function BlogPage() {
   return (
     <main className="bg-background">
       <BlogHero />
       <BlogList />
+      <BlogShowcase />
       <Cta />
     </main>
   );
