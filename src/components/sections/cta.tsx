8e4844153e7f65078b14acee04fc04a3761554eb
deepak rankawat <deepakrankawatcode@gmail.com>
2025-11-09 05:16:15 +0000

    remove it only this json animtiom (_for element div_)

--- src/components/sections/cta.tsx
+++ src/components/sections/cta.tsx
@@ -3,14 +3,13 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { FadeIn } from '@/components/animations/fade-in';
-import { LawBalanceLottie } from '../animations/law-balance-animation';
 
 export function Cta() {
   return (
     <section className="bg-secondary py-12 sm:py-16 text-foreground">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <FadeIn>
-          <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center text-center md:text-left bg-background p-12 gap-8">
+          <div className="grid grid-cols-1 items-center justify-center text-center md:text-left bg-background p-12 gap-8">
               <div>
                 <h2 className="mt-4 font-lato text-3xl md:text-4xl font-bold text-primary">
                   Ready to Optimize Your Legal Workflow?
@@ -22,14 +21,9 @@ export function Cta() {
                   <Link href="/contact">Request a Consultation</Link>
                 </Button>
               </div>
-               <div className="relative h-64 w-64 justify-self-center">
-                <LawBalanceLottie />
-              </div>
           </div>
         </FadeIn>
       </div>
     </section>
   );
 }
-
-    
