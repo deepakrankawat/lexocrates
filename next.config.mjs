c84280530406893d2581b2abefd29e90e7d3b7ef
deepak rankawat <deepakrankawatcode@gmail.com>
2026-02-23 09:02:22 +0000

    last

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD:next.config.js
-  /* config options here */
-  output: 'standalone',
-=======
->>>>>>> 8b31f7a9f26e90712fd8444b232cb805e4a76c23:next.config.mjs
+
   typescript: {
     ignoreBuildErrors: true,
   },
