ca28fad8f7d76f66fa1990a9e5ce7bdaba3b779a
deepak rankawat <deepakrankawatcode@gmail.com>
2026-03-21 08:39:19 +0000

    removie ti   (_for element div_)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,5 @@
 
 import { AboutHero } from '@/components/sections/about-hero';
-import { AboutIntro } from '@/components/sections/about-intro';
 import { VisionMission } from '@/components/sections/vision-mission';
 import { Welcome } from '@/components/sections/welcome';
 import { Certifications } from '@/components/sections/certifications';
@@ -13,7 +12,6 @@ export default function AboutPage() {
     <main className="bg-background">
       <AboutHero />
       <div className="mx-auto w-full px-6 sm:px-12 lg:px-24 max-w-[1400px] fhd:max-w-[1600px] qhd:max-w-[1800px]">
-        <AboutIntro />
         <Experience />
         <CoreValues />
         <Welcome />
