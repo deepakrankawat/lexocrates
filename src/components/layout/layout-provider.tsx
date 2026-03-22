611bde38bb076cb0043429b626c7027914946cf7
deepak rankawat <deepakrankawatcode@gmail.com>
2026-03-22 11:14:44 +0000

    remove it not use it  and it like a  (_for element div_)

--- src/components/layout/layout-provider.tsx
+++ src/components/layout/layout-provider.tsx
@@ -3,7 +3,6 @@
 import { usePathname } from 'next/navigation';
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
-import { Breadcrumbs } from '@/components/ui/breadcrumbs';
 
 export function LayoutProvider({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -12,9 +11,6 @@ export function LayoutProvider({ children }: { children: React.ReactNode }) {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow pt-[96px] sm:pt-[100px] lg:pt-[112px] bg-background">
-        <div className="mx-auto w-full px-6 sm:px-12 lg:px-24 max-w-[1800px] pt-6">
-          <Breadcrumbs />
-        </div>
         {children}
       </main>
       <Footer />
